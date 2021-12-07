fa3675606da9c30002ca5cea527bccd50d7e95e2
Mohamed Ali <mali1@Mohameds-MacBook-Air.local>
2021-12-07 00:51:34 -0500

    fopen

--- fs.c
+++ fs.c
@@ -4,10 +4,34 @@
 #include <stdint.h>
 #include "util.h"
 
+#define DELIM "/"
+#define MAXLEN 20
+
 int f_open(char *);
 int f_read(int, int, void*);
 
+int main(){
+	f_open("/usr/usr2/file.txt");
+	return 0;
+}
+
 int f_open(char * file){
+	char *token = strtok(file, DELIM);
+	char tokens[MAXLEN][MAXLEN];
+	int idx = 0;
+	// loop through the string to extract all other tokens
+   	while( token != NULL ) {
+      	printf( " %s\n", token ); //printing each token
+		strcpy(tokens[idx], token);
+		idx++;
+      	token = strtok(NULL, DELIM);
+   	}
+	// Print the list of tokens
+    printf("Token List:\n");
+    for (int i=0; i < idx; i++) {
+        printf("%s\n", tokens[i]);
+    }
+	return 0;
 	/*search dentry based on name*/
 	/*retreive inode number*/
 	/*choose the lowest unused fd*/
@@ -21,4 +45,5 @@ int f_read(int fd, int bytes, void* buffer){
 	/*read the required bytes and error if it violates the file size*/
 	/*put the bytes in buffer*/
 	/*return the number of bytes read*/
+	return 0;
 }
