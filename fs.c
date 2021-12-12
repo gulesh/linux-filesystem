f97d1896810756ba9f4547cce38111536cedbda7
Mohamed Ali <mali1@Mohameds-MacBook-Air.local>
2021-12-11 21:20:42 -0500

    open

--- fs.c
+++ fs.c
@@ -4,18 +4,69 @@
 #include <stdint.h>
 #include "util.h"
 
+#define INODEOFFSET (BLOCKSIZE + sb->inode_offset * BLOCKSIZE)
+#define DATAOFFSET  (BLOCKSIZE + sb->data_offset  * BLOCKSIZE)
+
 #define DELIM "/"
 #define MAXLEN 20
+#define ROOT 0
+
+int init_flag = 0;
+extern int open_fd_table[FD_TABLE_SIZE][FD_MAX];
 
 int f_open(char *);
 int f_read(int, int, void*);
 
 int main(){
-	f_open("/usr/usr2/file.txt");
+	f_open("file.txt");
 	return 0;
+}
+/*returns the inode offset of the directory with name 'name' in datablock with offset 'o'*/
+int get_inode(char* name, int o){
+	dentry *temp = malloc(sizeof(dentry);
+	temp = (dentry *)(DATAOFFSET + o);
+	int len = strlen(name);
+	printf("len: %d\n", len);
+	do{
+		if (len == temp->length){
+			char temp_name[temp->length];
+			temp_name = (char *) (&temp + sizeof(dentry));
+			if (strcmp(name, temp_name) == 0){
+				return temp->n;
+			}
+		}
+		temp = (dentry *)(&temp + temp->size);
+	}while(temp->last != 1);
+	return -1;
+}
+
+int get_block(int n){
+	inode *temp_inode = (inode *) (INODEOFFSET + n);
+	return temp_inode->dblocks[0];
+}
+
+int get_fd(int n){
+	for (int i = 0; i< FD_TABLE_SIZE; i++){
+		if (open_fd_table[i][FD_INODE] == -1){
+        	open_fd_table[i][FD_INODE] = n;
+			return i;
+		}
+	}
+	return -1;
+}
+
+void init_library(){
+	for (int i = 0; i< FD_TABLE_SIZE; i++){
+		open_fd_table[i][FD_INODE] = -1;
+		open_fd_table[i][FD_SEEK_POS] = 0;
+	}
 }
 
 int f_open(char * file){
+	if (init_flag == 0){
+		init_library();
+		init_flag = 1;
+	}
 	char *token = strtok(file, DELIM);
 	char tokens[MAXLEN][MAXLEN];
 	int idx = 0;
@@ -31,7 +82,25 @@ int f_open(char * file){
     for (int i=0; i < idx; i++) {
         printf("%s\n", tokens[i]);
     }
-	return 0;
+	/*current data offset (that of root)*/
+	int curr_d = 0;
+	/*current inode offset (that of node)*/
+	int curr_i = 0;
+	for (int i = 0; i<idx-1; i++){
+		if(curr_i = get_inode(tokens[i], curr_d) == -1){
+			printf("%s: No such file or directory\n", name);
+			exit(EXIT_FAILURE);
+		}
+		if(curr_d = get_block(curr_i) == -1)
+			exit(EXIT_FAILURE);
+	}
+	int f_inode = get_inode(tokens[idx-1], curr_d);
+	int fd = get_fd(f_inode);
+	if (fd == -1){
+		printf("Open fd table is full\n");
+		exit(EXIT_FAILURE);
+	}
+	return fd;
 	/*search dentry based on name*/
 	/*retreive inode number*/
 	/*choose the lowest unused fd*/
