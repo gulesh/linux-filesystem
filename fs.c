539dd97de48935399bac7bf0590cae6810f01477
mali1 <mali1>
2021-12-13 22:37:50 -0500

    seeking works for read

--- fs.c
+++ fs.c
@@ -289,7 +289,8 @@ int f_read(int fd, int bytes, void* buffer){
 			}
 		}
 	}
-
+	open_fd_table[fd][FD_SEEK_POS] = seek_pos + bytes;
+	
 	/*get inode from table using fd*/
 	/*got to data block and seek to position from table*/
 	/*read the required bytes and error if it violates the file size*/
